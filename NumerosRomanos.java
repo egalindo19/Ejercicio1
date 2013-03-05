533d860d0f4c1b74d686855abeab61f1df82c082
egalindo <egalindo@sintelti.com.mx>
2013-03-04 19:18:06 -0600

    Ejercicio Numeros Romanos

---
+++ NumerosRomanos.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class NumerosRomanos{
+	Scanner teclado = new Scanner(System.in);
+	int num;
+	String numRom;
+	int tam = romano.length;
+	
+	public static void main(String []args){
+	
+	System.out.println("Ingresar numero a convertir:");
+	num= teclado.nextInt();
+	
+		 if(tam == 4) {
+   
+     switch(romano){
+     case '1':
+     System.out.print("M");
+     break;
+     case '2':
+     System.out.print("MM");
+     break;
+     case '3':
+     System.out.print("MMM");
+     break;
+     }
+			
+		}
+		System.out.println("El numero romano es:"+romano);
+		
+		}
+		
+		}
+
+}
