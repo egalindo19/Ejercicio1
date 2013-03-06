057c43cdd9c544ca564afc71e37d5c71d6ba8d36
egalindo <egalindo@sintelti.com.mx>
2013-03-05 19:49:33 -0600

    funcional de numeros romanos a numeros arabigos

--- NumerosRomanos.java
+++ NumerosRomanos.java
@@ -1,35 +1,55 @@
 import java.util.Scanner;
 
 public class NumerosRomanos{
-	Scanner teclado = new Scanner(System.in);
-	int num;
-	String numRom;
-	int tam = romano.length;
 	
-	public static void main(String []args){
+	public static void main (String []args){
 	
-	System.out.println("Ingresar numero a convertir:");
-	num= teclado.nextInt();
-	
-		 if(tam == 4) {
-   
-     switch(romano){
-     case '1':
-     System.out.print("M");
-     break;
-     case '2':
-     System.out.print("MM");
-     break;
-     case '3':
-     System.out.print("MMM");
-     break;
-     }
+		Scanner teclado = new Scanner(System.in);
+		String numRom;
+		int sumador=0;
+		int length;
+		int[] arreglo;
+		
+		System.out.println("Ingresar numero romano con MAYUSCULAS:");
+		numRom = teclado.nextLine();
+		length = numRom.length();
+		arreglo = new int[length +1];
+		
+		for(int i=0; i < length; i++){
+			char letra;
+			letra = numRom.charAt(i);
 			
+			if(letra == 'I'){
+				arreglo[i] = 1;
+			}
+			if(letra == 'V'){
+				arreglo[i] = 5;
+			}
+			if(letra == 'X'){
+				arreglo[i] = 10;
+			}
+			if(letra == 'L'){
+				arreglo[i] = 50;
+			}
+			if(letra == 'C'){
+				arreglo[i] = 100;
+			}
+			if(letra == 'D'){
+				arreglo[i] = 500;
+			}
+			if(letra == 'M'){
+				arreglo[i] = 1000;
+			}
 		}
-		System.out.println("El numero romano es:"+romano);
-		
+		for(int i=0;i < length;i++){
+			if(arreglo[i] < arreglo[i + 1]){
+				sumador = sumador + (arreglo[i + 1] - arreglo[i]);
+				i++;
+			}else{
+				sumador = sumador + arreglo[i];
+			}
 		}
-		
-		}
-
+		System.out.println("El numero arabigo es:"+sumador);
+	}
+	
 }
