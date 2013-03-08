bd46b7e96c3da00a71b50cc4bf2e6a24c14624a0
egalindo <egalindo@sintelti.com.mx>
2013-03-08 17:21:27 -0600

    Se sube actualizacion de programa

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,23 +1,36 @@
 
 public class Fibonacci{
+          
+    int a=0,b=1,c,par,suma=0; 
+	int []vec;  
+	
+    public void pares(){ 
+		System.out.println("Los numeros fibonaccis: "); 
+			for(int i=0;i<30;i++){ 
+				c=a+b; 
+				a=b; 
+				b=c; 
+				par=c%2; 
+                suma=suma+c;
+				
+				if (par==0){
+					System.out.println("Numeros pares:"+c);
+					
+					
+				}
+			}
+	}	
 
-public static void main(String[] args) { 
-       
-         
-       int a=0,b=1,c,par; 
-       System.out.print("Los numeros fibonaccis: "); 
-       for(int i=0;i<=10;i++){ 
-       c=a+b; 
-       a=b; 
-       b=c; 
-       par=c%2; 
-        
-      
-           
-		if (par==0){
-		System.out.println("numeros pares:"+c);
-		}
+	public void suma(){
 		
-        } 
+        System.out.println("La suma de los elementos es:"+suma);
+	
+	}
+
+	public static void main(String[] args) { 
+		Fibonacci fb = new Fibonacci();
+		fb.pares();
+		fb.suma();
+		
 	}
 }
